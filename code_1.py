f9ec3758f8b8bce3423d3c1d952da7a2d7d7186f
cryptonewsre <139812145+cryptonewsre@users.noreply.github.com>
2023-09-25 07:10:14 +0200

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,14 @@
+    main()
+
+
+def main():
+if __name__ == "__main__":
+
+import random
+    data = generate_random_data()
+
+        print(f"Random Number: {item}")
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+def generate_random_data():
+    for item in data:
