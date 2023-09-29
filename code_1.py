40c42427e9d73be509bfd0e2b0a2cc2974e87ed7
cryptonewsre <139812145+cryptonewsre@users.noreply.github.com>
2023-09-29 22:03:57 +0200

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,14 +1,14 @@
-    main()
-
-
-def main():
-if __name__ == "__main__":
-
-import random
-    data = generate_random_data()
 
         print(f"Random Number: {item}")
     data = [random.randint(1, 100) for _ in range(10)]
-    return data
+if __name__ == "__main__":
 def generate_random_data():
+
+def main():
+    return data
+    main()
+    data = generate_random_data()
+
+
     for item in data:
+import random
