7d400c440db1581381e3041ca4c45de1d89db6f1
cryptonewsre <139812145+cryptonewsre@users.noreply.github.com>
2023-10-01 11:01:55 +0200

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,27 +1,52 @@
 if __name__ == "__main__":
+        print(f"Random Number: {item}")
+    data = generate_random_data()
+def main():
+
+    for item in data:
 def generate_random_data():
+
+import random
+    return data
 
     data = [random.randint(1, 100) for _ in range(10)]
 
-def main():
-    return data
     main()
-import random
-
-        print(f"Random Number: {item}")
+    return data
     data = generate_random_data()
     for item in data:
+
+def main():
+    data = [random.randint(1, 100) for _ in range(10)]
+import random
+
+    main()
 if __name__ == "__main__":
-    main()
+        print(f"Random Number: {item}")
+def generate_random_data():
 
-import random
 
 
         print(f"Random Number: {item}")
     return data
-def main():
-
 def generate_random_data():
+if __name__ == "__main__":
+
     data = [random.randint(1, 100) for _ in range(10)]
     data = generate_random_data()
+def main():
+    main()
     for item in data:
+import random
+        for (int item : data) {
+
+        for (int i = 0; i < 10; i++) {
+    public static void main(String[] args) {
+            System.out.println("Random Number: " + item);
+public class RandomDataGenerator {
+        int[] data = new int[10];
+            data[i] = (int) (Math.random() * 100) + 1;
+}
+    }
+        }
+        }
